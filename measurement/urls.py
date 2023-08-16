40b3b3d14869b0c32b3aa43457d258df84b9bef5
Danil Krutikov <kdanil.i@yandex.ru>
2023-08-16 22:39:19 +0700

    Second commit

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 
-from .views import New_sensorView, Patch_sensor, Post_measurement, Measurement
+from .views import New_sensorView, Patch_sensorView, Post_measurementView, Measurement, SensorsView, SensorView
 
 urlpatterns = [
-    path('new_sensor/<name>/<description>/', New_sensorView.as_view()),
-    path('patch_sensor/<sensor>/<description>/', Patch_sensor.as_view()),
-    path('post_measurement/<sensor>/<temperature>/', Post_measurement.as_view()),
-    path('measurement/<pk>/', Measurement.as_view()),
+    path('new_sensor/', New_sensorView.as_view()),
+    path('patch_sensor/<pk>/', Patch_sensorView.as_view()),
+    path('post_measurement/', Post_measurementView.as_view()),
+    path('sensors/', SensorsView.as_view()),
+    path('sensor/<pk>/', SensorView.as_view()),
 
 ]
 
--- measurement/views.py
+++ measurement/views.py
@@ -1,34 +1,38 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
-from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, CreateAPIView, ListAPIView
 from rest_framework.response import Response
 
 from .models import Sensor, Measurement
 from .serializers import SensorDetailSerializer, MeasurementSerializer
 
-
+# создаем датчик
 class New_sensorView(ListCreateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorDetailSerializer
-    def post(self, request, name, description):
-        Sensor(name=name, description=description).save()
-        return Response({'status': 'OK'})
 
-class Patch_sensor(ListCreateAPIView):
+# обновляем датчик
+class Patch_sensorView(RetrieveAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorDetailSerializer
-    def patch(self, sensor, description):
-        Sensor(pk=sensor, description=description).save()
-        return Response({'status': 'OK'})
+    def patch(self, request):
+        request.serializer_class.save()
+        return Response({'status': 'ok'})
 
-class Post_measurement(ListCreateAPIView):
+#  добавление измерения
+class Post_measurementView(CreateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = MeasurementSerializer
-    def post(self, sensor, temperature):
-        Measurement(pk=sensor, temperature=temperature).save()
+    def post(self, request):
+        request.serializer_class.save()
         return Response({'status': 'OK'})
 
+# получение датчиков
+class SensorsView(ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
 
-class Measurement(RetrieveAPIView):
+# получение информации по датчику
+class SensorView(RetrieveAPIView):
     queryset = Sensor.objects.all()
     serializer_class = MeasurementSerializer
